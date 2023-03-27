570790d1517e43b233cc38e683c538ba852bb30d
Ashu Kumar <95637428+Ashu-kumar40@users.noreply.github.com>
2023-03-27 08:14:27 +0000

    Submit button added to the form

--- src/Components/HandelingUserInput.js
+++ src/Components/HandelingUserInput.js
@@ -4,23 +4,43 @@ class HandelingUserInput extends Component {
     firstname: "firstname",
     lastname: "lastname",
   };
-  HandleFirstnameChange = (event) =>{
+  HandleFirstnameChange = (event) => {
     this.setState({
-        firstname: event.target.value
-    })
-  }
-  HandleLastnameChange = (event) =>{
+      firstname: event.target.value,
+    });
+  };
+  HandleLastnameChange = (event) => {
     this.setState({
-        lastname: event.target.value
-    })
-  }
+      lastname: event.target.value,
+    });
+  };
+  handleSubmit = (event) => {
+    event.preventDefault();
+    // the above function is used to prevent the default behaviour of the form. if you submit the form it will refresh the page. so to prevent that we use this function.
+    console.log({
+      fname: this.state.firstname,
+      lname: this.state.lastname,
+    });
+    alert(
+      `Firstnama : ${this.state.firstname} \nLastname: ${this.state.lastname}`
+    );
+  };
   render() {
     return (
       <div>
         <h1>Form</h1>
-        <form>
-          <input type="text" value={this.state.firstname} onChange={this.HandleFirstnameChange}></input>
-          <input type="text" value={this.state.lastname} onChange={this.HandleLastnameChange}></input>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            value={this.state.firstname}
+            onChange={this.HandleFirstnameChange}
+          ></input>
+          <input
+            type="text"
+            value={this.state.lastname}
+            onChange={this.HandleLastnameChange}
+          ></input>
+          <button type="submit">Submit</button>
         </form>
       </div>
     );
